d6f4004c11b176712fe20f09a60e4d68b4bd8425
Dante <maksim.onlainov@yandex.ru>
2022-10-05 17:25:51 +0300

    update base.html

--- travel/views.py
+++ travel/views.py
@@ -5,3 +5,7 @@ def home(request):
     name = 'Bob'
 
     return render(request, 'home.html', {'name': name})
+def about(request):
+    name = 'about my first project'
+
+    return render(request, 'about.html', {'name': name})
